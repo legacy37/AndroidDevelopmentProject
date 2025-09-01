15c00f4534c89764260dbcae17766b8209fd310b
Vladislav <vpevdokimov0@gmail.com>
2025-09-01 20:20:35 +0300

    Added button. For now it shows on top left corner over everything

--- app/src/main/java/com/example/androiddevelopmentproject/MainActivity.kt
+++ app/src/main/java/com/example/androiddevelopmentproject/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,9 +22,10 @@ class MainActivity : ComponentActivity() {
             AndroidDevelopmentProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Vladislav",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    FirstButton()
                 }
             }
         }
@@ -36,6 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name, welcome to Android!",
         modifier = modifier
     )
+
 }
 
 @Preview(showBackground = true)
@@ -43,5 +46,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     AndroidDevelopmentProjectTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun FirstButton()
+{
+    Button(onClick = { println("Button pressed!")})
+    {
+        Text("Button")
     }
 }
